6a318494e46925ab8c439d3cbd00801b7566afa7
Jules LASNE <jlasne@e1r12p13.42.fr>
2016-07-11 07:32:30 +0200

    commit

--- d05/ex01/ft_strncpy.c
+++ d05/ex01/ft_strncpy.c
@@ -6,11 +6,52 @@
 /*   By: jlasne <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/07/11 07:22:07 by jlasne            #+#    #+#             */
-/*   Updated: 2016/07/11 07:24:01 by jlasne           ###   ########.fr       */
+/*   Updated: 2016/07/11 07:32:24 by jlasne           ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/* ************************************************************************** */i
+
+int		ft_strlen(char *str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (str[i] != 0)
+	{
+		i++;
+		j++;
+	}
+	return (j);
+}
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	
+	int i;
+	int j;
+
+	i = 0;
+	j = ft_strlen(src);
+	if (j < n)
+	{
+		while(src[i] != '\0')
+		{
+			dest[i] = src[i];
+			i++;
+			n--;
+		}
+		while(n >= 0)
+		{
+			dest[i] = '\0';
+			i++
+		}
+
+	}
+	while (n > 0)
+	{
+		dest[i] = src[i];
+		i++;
+		n--;
+	}
+	dest[i] = '\0';
 }
